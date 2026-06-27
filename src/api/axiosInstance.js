ee0a9589caabb93f554298168e85dd737ecc473d
BaelNguyen <amn1022004@gmail.com>
2026-06-27 16:02:19 +0700

    endpoint

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://shift-hundreds-adsl-belle.trycloudflare.com',
   withCredentials: true,
 })
 
